7f5f7c9441987537923486cd67e0f31bc14ffe4b
tsas001 <tsas.com.vn@gmail.com>
2021-09-16 15:44:30 +0700

    Add Product_infomation model, controller, view

---
+++ Book-Shop/resources/views/backend/product_infomation/create.blade.php
@@ -0,0 +1,34 @@
+@extends('backend.layouts.main')
+
+@section('content')
+    <div class="container">
+        <form method="POST" action="{{url('admin/product_infomations')}}">
+            @csrf
+            <div class="form-group">
+                <label>Name:</label>
+                <input type="text" name="name" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label>Author:</label>
+                <input type="text" name="author" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label>Published:</label>
+                <input type="text" name="published" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label>Language:</label>
+                <input type="text" name="language" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label>Year:</label>
+                <input type="text" name="year" class="form-control"/>
+            </div>
+            <div class="form-group">
+                <label>Product_Id:</label>
+                <input type="text" name="product_id" class="form-control"/>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+@endsection
---
+++ Book-Shop/resources/views/backend/product_infomation/index.blade.php
@@ -0,0 +1,48 @@
+@extends('backend.layouts.main')
+
+@section('title')
+    Sub Categories
+@endsection
+
+@push('css')
+    <link rel="stylesheet" href="#" />
+@endpush
+
+@push('js')
+
+@endpush
+
+@section('content')
+    <table class="table table-striped table-dark">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Author</th>
+            <th>Published</th>
+            <th>Language</th>
+            <th>Year</th>
+            <th>Product_id</th>
+            <th>Action</th>
+        </tr>
+        @foreach ($product_infomations as $product_infomation)
+            <tr>
+                <td>{{$product_infomation->id}}</td>
+                <td>{{$product_infomation->name}}</td>
+                <td>{{$product_infomation->author}}</td>
+                <td>{{$product_infomation->published}}</td>
+                <td>{{$product_infomation->language}}</td>
+                <td>{{$product_infomation->year}}</td>
+                <td>{{$product_infomation->product_id}}</td>
+                <td>
+                    <a class="btn btn-primary" href="{{ url('admin/sub_categories/'.$product_infomation->id)}}">Show</a>
+                    <a class="btn btn-primary" href="{{ url('admin/sub_categories/'.$product_infomation->id.'/edit')}}">Edit</a>
+                    <form method="POST" action="{{url('admin/sub_categories/'.$product_infomation->id)}}" class="d-inline" onsubmit="return confirm('Are you sure?')">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger" type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+@endsection
